051fe24944a2b1afad8d05b0dc619f4d4a821c8f
Rafalp16 <rafalp16a@gmail.com>
2017-03-14 20:50:47 +0100

    Fix paths.js

--- toz-fe/src/router/index.js
+++ toz-fe/src/router/index.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Transfer from '@/components/Transfer'
 import paths from './paths'
 
 Vue.use(Router)
 
-export default new Router(paths)
+export default new Router({
+  routes: [
+    {
+      path: paths.transfer,
+      name: 'transfer',
+      component: Transfer
+    },
+    {
+      path: paths.home,
+      name: 'home',
+      component: {
+        template: '<router-link to="/transfer">Kliknij tutaj, aby wyświetlić dane do przelewu</router-link>'
+      }
+    }
+  ]
+})
--- toz-fe/src/router/paths.js
+++ toz-fe/src/router/paths.js
@@ -1,18 +1,4 @@
-import Transfer from '@/components/Transfer'
+const transfer = '/transfer'
+const home = '/'
 
-export default {
-  routes: [
-    {
-      path: '/transfer',
-      name: 'transfer',
-      component: Transfer
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: {
-        template: '<router-link to="/transfer">Kliknij tutaj, aby wyświetlić dane do przelewu</router-link>'
-      }
-    }
-  ]
-}
+export default { transfer, home }
