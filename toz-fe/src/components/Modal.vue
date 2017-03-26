104d05b8ab21164de8892d894df9ba88f214d62e
Bartek Wielgosz <wielgoszbartek@gmail.com>
2017-03-26 17:11:54 +0200

    Added pets list

---
+++ toz-fe/src/components/Modal.vue
@@ -0,0 +1,92 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+            </slot>
+          </div>
+          <button class="modal-default-button" @click="$emit('close')">
+            OK
+          </button>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Modal'
+}
+</script>
+
+<style>
+.modal-mask {
+  position: absolute;;
+  z-index: 9999;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  min-width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
---
+++ toz-fe/src/components/PetsList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="posrel">
+  <carousel :navigationEnabled="true" :paginationEnabled="false" :navigationClickTargetSize="0" :perPageCustom="[[768, 3], [1024, 5]]">
+    <slide v-for="pet of pets[0]" :key="pet.name" >
+      <div @click="openModal(pet)">
+        <img :src="pet.profilePic" alt="pet photo">
+        <h2>Imię: {{ pet.name }}</h2>
+        <h2>Typ: {{ pet.type }}</h2>
+      </div>
+    </slide>
+  </carousel>
+  <modal v-if="showModal" @close="showModal = false">
+    <h1 slot="header">{{ currentPet.name }}</h1>
+    <h2 slot="header">{{ currentPet.description }}</h2>
+  </modal>
+  <router-link to="/transfer">Kliknij tutaj, aby wyświetlić dane do przelewu</router-link>
+</div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel'
+import Modal from './Modal.vue'
+
+export default {
+  data () {
+    return {
+      pets: [],
+      errors: [],
+      showModal: false,
+      currentPet: {}
+    }
+  },
+  created () {
+    this.$http.get(`https://private-anon-1d69436fab-zwierzakiandroid.apiary-mock.com/pets`)
+    .then(response => {
+      this.pets.push(response.data)
+    })
+    .catch(error => {
+      this.errors.push(error)
+    })
+  },
+  methods: {
+    openModal (pet) {
+      this.showModal = true
+      this.currentPet.name = pet.name
+      this.currentPet.description = pet.description
+    }
+  },
+  components: {
+    Carousel,
+    Slide,
+    Modal
+  }
+}
+</script>
+<style>
+.VueCarousel {
+  width: 90%;
+  margin: 0 auto;
+}
+</style>
