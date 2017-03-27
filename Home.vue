bddda6ea33cc431867b38bebaa0c96ddb90fb3ba
Tomasz Wołoszyk <Tomek1592@gmail.com>
2017-03-27 21:22:55 +0200

    update/versatile 'News' component

---
+++ Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <router-link to="/transfer">Kliknij tutaj, aby wyświetlić dane do przelewu</router-link>
+    <News></News>
+  </div>
+</template>
+
+<script>
+  import News from './News'
+  export default {
+    name: 'Home',
+    data () {
+      return {
+      }
+    },
+    components: {
+      News
+    }
+  }
+</script>
+
+<style scoped>
+</style>
