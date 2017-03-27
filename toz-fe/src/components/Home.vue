bd88fc9b8dd2639fd13662bc8209c0549201610b
Tomasz Wołoszyk <Tomek1592@gmail.com>
2017-03-27 21:37:03 +0200

    update/Home

--- toz-fe/src/components/Home.vue
+++ toz-fe/src/components/Home.vue
@@ -1,20 +1,7 @@
 <template>
   <div>
     <router-link to="/transfer">Kliknij tutaj, aby wyświetlić dane do przelewu</router-link>
-    <div class="news-container" v-for="item in news">
-      <router-link :to="{ name: 'news', params: {
-        id: item.id,
-        title: item.title,
-        longContent: item.longContent,
-        dateDay: item.date.day,
-        dateMonth: item.date.month,
-        dateYear: item.date.year }
-      }">
-        <h1>{{ item.title }}</h1>
-      </router-link>
-      <h3>{{ item.date.day }}-{{ item.date.month }}-{{ item.date.year }}</h3>
-      <h3>{{ item.shortContent }}</h3>
-    </div>
+    <News></News>
   </div>
 </template>
 
@@ -24,30 +11,6 @@
     name: 'Home',
     data () {
       return {
-        news: [
-          {
-            id: 1,
-            title: 'Komunikat 1',
-            shortContent: 'Skrócona treść komunikatu 1.',
-            longContent: 'Długa treść komunikatu 1.',
-            date: {
-              day: '01',
-              month: '01',
-              year: '2017'
-            }
-          },
-          {
-            id: 2,
-            title: 'Komunikat 2',
-            shortContent: 'Skrócona treść komunikatu 2.',
-            longContent: 'Długa treść komunikatu 2.',
-            date: {
-              day: '02',
-              month: '01',
-              year: '2017'
-            }
-          }
-        ]
       }
     },
     components: {
@@ -57,8 +20,4 @@
 </script>
 
 <style scoped>
-  .news-container {
-    border: 1px solid black;
-    width: 500px;
-    height: 200px;
-  }</style>
+</style>
