2f67cd1a09315a7596dfc7ab2cde1b3cb7a493f6
Bartek Wielgosz <wielgoszbartek@gmail.com>
2017-03-30 13:17:22 +0200

    added missing PetDetails component

---
+++ toz-fe/src/components/PetDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Name: {{$route.params.name}}</h2>
+    <h2>Description: {{$route.params.description}}</h2>
+    <router-link to='/pets'>Go back</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+</script>
+<style>
+
+</style>
