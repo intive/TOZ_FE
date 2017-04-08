7fc344230ba0fa32b7e8985e695abe33b8fd59c0
Jakcek Placek <Jakcek Placek>
2017-04-08 22:09:15 +0200

    Changes in routing to fix disapearing data

--- toz-fe/src/main.js
+++ toz-fe/src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import Modal from './components/Modal.vue'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
+Vue.component('Modal', Vue.extend(Modal))
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- toz-fe/src/router/paths.js
+++ toz-fe/src/router/paths.js
@@ -1,5 +1,5 @@
 const transfer = '/transfer'
 const home = '/'
 const pets = '/pets'
-const petDetails = '/petDetails/:id'
+const petDetails = '/petDetails/id-:id/name-:name/description-:description/sex-:sex/created-:created/type-:type'
 export default { transfer, home, pets, petDetails }
