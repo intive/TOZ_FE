528ab1de8d9dd4043479a30bd4713d8184b182ab
Rafał Pytel <rafalp16a@gmail.com>
2017-04-12 21:53:31 +0200

    Add small visual changes to button and code

--- toz-fe/src/components/Account.vue
+++ toz-fe/src/components/Account.vue
@@ -1,21 +1,21 @@
 <template>
-<form v-on:submit.prevent="handleSubmit" class="col-md-8 col-lg-4">
-  <div class="form-group">
-   <label for="oldPassword">{{ $t("account.oldPassword") }}</label>
-   <input type="password" class="form-control" id="oldPassword" placeholder="Old password" maxlength="24" required>
- </div>
- <div class="form-group">
-  <label for="newPassword">{{ $t("account.newPassword") }}</label>
-  <input type="password" class="form-control" id="newPassword" placeholder="New password" maxlength="24" required>
-</div>
-<div class="form-group">
- <label for="confirmNewPassword">{{ $t("account.confirmNewPassword") }}</label>
- <input type="password" class="form-control" id="confirmNewPassword" placeholder="Confirm new password" maxlength="24" required>
-</div>
-<div class="form-group">
- <input type="submit" class="form-control" value="OK">
-</div>
-</form>
+  <form v-on:submit.prevent="handleSubmit" class="col-md-8 col-lg-4">
+    <div class="form-group">
+      <label for="oldPassword">{{ $t("account.oldPassword") }}</label>
+      <input type="password" class="form-control" id="oldPassword" placeholder="Old password" maxlength="24" required>
+    </div>
+    <div class="form-group">
+      <label for="newPassword">{{ $t("account.newPassword") }}</label>
+      <input type="password" class="form-control" id="newPassword" placeholder="New password" maxlength="24" required>
+    </div>
+    <div class="form-group">
+      <label for="confirmNewPassword">{{ $t("account.confirmNewPassword") }}</label>
+      <input type="password" class="form-control" id="confirmNewPassword" placeholder="Confirm new password" maxlength="24" required>
+    </div>
+    <div class="form-group">
+      <input type="submit" class="btn btn-outline-success my-2" value="OK">
+    </div>
+  </form>
 </template>
 
 
