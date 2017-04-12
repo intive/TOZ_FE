15f16ee3c15b5c0a774a212820cdb21701f23598
Rafał Pytel <rafalp16a@gmail.com>
2017-04-12 20:36:50 +0200

    Add account tab + password change

---
+++ toz-fe/src/components/Account.vue
@@ -0,0 +1,39 @@
+<template>
+<form v-on:submit.prevent="handleSubmit" class="col-md-8 col-lg-4">
+  <div class="form-group">
+   <label for="oldPassword">{{ $t("account.oldPassword") }}</label>
+   <input type="password" class="form-control" id="oldPassword" placeholder="Old password" maxlength="24" required>
+ </div>
+ <div class="form-group">
+  <label for="newPassword">{{ $t("account.newPassword") }}</label>
+  <input type="password" class="form-control" id="newPassword" placeholder="New password" maxlength="24" required>
+</div>
+<div class="form-group">
+ <label for="confirmNewPassword">{{ $t("account.confirmNewPassword") }}</label>
+ <input type="password" class="form-control" id="confirmNewPassword" placeholder="Confirm new password" maxlength="24" required>
+</div>
+<div class="form-group">
+ <input type="submit" class="form-control" value="OK">
+</div>
+</form>
+</template>
+
+
+<script>
+
+export default {
+  name: 'Account',
+  data () {
+    return {}
+  },
+  methods: {
+    handleSubmit () {
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- toz-fe/src/router/paths.js
+++ toz-fe/src/router/paths.js
@@ -3,5 +3,6 @@ const home = '/'
 const pets = '/pets'
 const petDetails = '/petDetails/id-:id/name-:name/description-:description/sex-:sex/created-:created/type-:type'
 const news = '/news/:id'
+const account = '/account'
 
-export default { transfer, home, news, pets, petDetails }
+export default { transfer, home, news, pets, petDetails, account }
