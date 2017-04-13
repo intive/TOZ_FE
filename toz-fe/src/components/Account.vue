c846f2374a3fe4ce34c48a3aa6d6169aa07bfcea
Rafał Pytel <rafalp16a@gmail.com>
2017-04-13 10:19:23 +0200

    Fix i18n

--- toz-fe/src/components/Account.vue
+++ toz-fe/src/components/Account.vue
@@ -2,18 +2,18 @@
   <form v-on:submit.prevent="handleSubmit" class="col-md-8 col-lg-4">
     <div class="form-group">
       <label for="oldPassword">{{ $t("account.oldPassword") }}</label>
-      <input type="password" class="form-control" id="oldPassword" placeholder="Old password" maxlength="24" required>
+      <input type="password" class="form-control" id="oldPassword" :placeholder="$t('account.oldPassword')" maxlength="24" required>
     </div>
     <div class="form-group">
       <label for="newPassword">{{ $t("account.newPassword") }}</label>
-      <input type="password" class="form-control" id="newPassword" placeholder="New password" maxlength="24" required>
+      <input type="password" class="form-control" id="newPassword" :placeholder="$t('account.newPassword')" maxlength="24" required>
     </div>
     <div class="form-group">
       <label for="confirmNewPassword">{{ $t("account.confirmNewPassword") }}</label>
-      <input type="password" class="form-control" id="confirmNewPassword" placeholder="Confirm new password" maxlength="24" required>
+      <input type="password" class="form-control" id="confirmNewPassword" :placeholder="$t('account.confirmNewPassword')" maxlength="24" required>
     </div>
     <div class="form-group">
-      <input type="submit" class="btn btn-outline-success my-2" value="OK">
+      <input type="submit" class="btn btn-outline-success my-2" :value="$t('button.ok')">
     </div>
   </form>
 </template>
