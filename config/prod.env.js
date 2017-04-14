9dc8a81ea9a767b3ac4cc57b0511e8ec8e2547a2
Piotr Zimoch <piotr.zimoch@blstream.com>
2017-04-14 10:43:53 +0200

    updated API url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://dev.patronage2017.intive-projects.com"'
+  //API_URL: '"http://dev.patronage2017.intive-projects.com"'
+  API_URL: '"https://intense-badlands-80645.herokuapp.com/"'
 }
