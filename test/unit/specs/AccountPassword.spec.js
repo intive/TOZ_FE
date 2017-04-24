c0a0518591ce6b80730f82cd21d770d2393a3686
Tomek1592 <Tomek1592@gmail.com>
2017-04-24 12:22:41 +0200

    add/unit test

---
+++ test/unit/specs/AccountPassword.spec.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import AccountPassword from '@/components/AccountPassword'
+
+describe('AccountPassword.vue', () => {
+  it('Should render proper old password group', () => {
+    const Constructor = Vue.extend(AccountPassword)
+    const vm = new Constructor().$mount()
+
+    expect(vm.$el.querySelector('#oldPasswordGroup label').textContent).to.equal('Stare hasło')
+    expect(vm.$el.querySelector('#oldPassword').placeholder).to.equal('Stare hasło')
+    expect(vm.$el.querySelector('#oldPassword').type).to.equal('password')
+  })
+
+  it('Should render proper new password group', () => {
+    const Constructor = Vue.extend(AccountPassword)
+    const vm = new Constructor().$mount()
+
+    expect(vm.$el.querySelector('#newPasswordGroup label').textContent).to.equal('Nowe hasło')
+    expect(vm.$el.querySelector('#newPassword').placeholder).to.equal('Nowe hasło')
+    expect(vm.$el.querySelector('#newPassword').type).to.equal('password')
+  })
+
+  it('Should render proper confirm new password group', () => {
+    const Constructor = Vue.extend(AccountPassword)
+    const vm = new Constructor().$mount()
+
+    expect(vm.$el.querySelector('#confirmNewPasswordGroup label').textContent).to.equal('Potwierdź nowe hasło')
+    expect(vm.$el.querySelector('#confirmNewPassword').placeholder).to.equal('Potwierdź nowe hasło')
+    expect(vm.$el.querySelector('#confirmNewPassword').type).to.equal('password')
+  })
+})
