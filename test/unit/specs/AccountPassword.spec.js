e2f23dea1ee8a379ce5caf77f4957f9a60ae85f4
Bartek Wielgosz <wielgoszbartek@gmail.com>
2017-05-13 18:14:21 +0200

    fixed unit tests

--- test/unit/specs/AccountPassword.spec.js
+++ test/unit/specs/AccountPassword.spec.js
@@ -6,16 +6,16 @@ describe('AccountPassword.vue', () => {
     const Constructor = Vue.extend(AccountPassword)
     const vm = new Constructor().$mount()
 
-    expect(vm.$el.querySelectorAll('.form-group label')[0].textContent).to.equal(vm.$t('account.oldPassword'))
-    expect(vm.$el.querySelectorAll('.form-group .form-control')[0].placeholder).to.equal(vm.$t('account.oldPassword'))
+    expect(vm.$el.querySelectorAll('.form-group label')[0].textContent).to.equal(vm.$t('accountPassword.oldPassword'))
+    expect(vm.$el.querySelectorAll('.form-group .form-control')[0].placeholder).to.equal(vm.$t('accountPassword.oldPassword'))
     expect(vm.$el.querySelectorAll('.form-group .form-control')[0].type).to.equal('password')
 
-    expect(vm.$el.querySelectorAll('.form-group label')[1].textContent).to.equal(vm.$t('account.newPassword'))
-    expect(vm.$el.querySelectorAll('.form-group .form-control')[1].placeholder).to.equal(vm.$t('account.newPassword'))
+    expect(vm.$el.querySelectorAll('.form-group label')[1].textContent).to.equal(vm.$t('accountPassword.newPassword'))
+    expect(vm.$el.querySelectorAll('.form-group .form-control')[1].placeholder).to.equal(vm.$t('accountPassword.newPassword'))
     expect(vm.$el.querySelectorAll('.form-group .form-control')[1].type).to.equal('password')
 
-    expect(vm.$el.querySelectorAll('.form-group label')[2].textContent).to.equal(vm.$t('account.confirmNewPassword'))
-    expect(vm.$el.querySelectorAll('.form-group .form-control')[2].placeholder).to.equal(vm.$t('account.confirmNewPassword'))
+    expect(vm.$el.querySelectorAll('.form-group label')[2].textContent).to.equal(vm.$t('accountPassword.confirmNewPassword'))
+    expect(vm.$el.querySelectorAll('.form-group .form-control')[2].placeholder).to.equal(vm.$t('accountPassword.confirmNewPassword'))
     expect(vm.$el.querySelectorAll('.form-group .form-control')[2].type).to.equal('password')
 
     expect(vm.$el.querySelectorAll('.form-group .btn')[0].value).to.equal(vm.$t('common.button.ok'))
