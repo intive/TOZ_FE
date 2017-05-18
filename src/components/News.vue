bd24cca07e788492b6d599ed291aa9ab7eccd147
Rafał Pytel <rafalp16a@gmail.com>
2017-05-18 15:51:41 +0200

    News redesign

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,30 +1,23 @@
 <template>
-  <div>
+  <div class="container">
     <loader v-if="loading"></loader>
-    <div class="container">
-      <div v-for="item of news" :key="item.id" class="news-item clearfix">
-        <div class="news-date">
-          {{ $t('news.added') }} {{ convertTimeStamp }}
-        </div>
-        <h2 class="news-title">
-          <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', shortened: false } }">
-            {{ item.title }}
-          </router-link>
-        </h2>
-        <div class="row">
-          <div class="col-md-5">
-            <img :src="item.photoUrl" alt="" class="news-image">
-          </div>
-          <div class="col-md-7 content-wrapper">
-            <p class="content"> {{ shortenContent(item) }} </p>
-            <div class="wrapper">
-              <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', shortened: false } }" class="btn">
-                {{ $t('news.readMore') }}
-              </router-link>
-            </div>
-          </div>
-        </div>
+    <div class="errors" v-if="errors.length">
+      <h2 v-for="error of errors">{{ error.message }}</h2>
+    </div>
+    <div v-for="item of news" :key="item.id" class="news-item clearfix">
+      <div class="news-date">
+        {{ $t('news.added') }} {{ convertTimeStamp }}
       </div>
+      <h2 class="news-title">
+        <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', shortened: false } }">
+          {{ item.title }}
+        </router-link>
+      </h2>
+      <img :src="item.photoUrl" alt="" class="news-image">
+      <p class="news-body"> {{ shortenContent(item) }} </p>
+      <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', shortened: false } }" class="btn btn-primary news-button">
+        {{ $t('news.readMore') }}
+      </router-link>
     </div>
   </div>
 </template>
@@ -32,32 +25,42 @@
 <script>
   import Loader from './Loader'
   import moment from 'moment'
+
   export default {
     name: 'News',
     data () {
       return {
         news: [{
-          'id': '9b6d936b-56d6-49b7-801f-c0d62b3adf6f',
+          'id': 'd8728661-d429-4e96-893a-75c0f89af76e',
           'title': 'Zostań naszym wolontariuszem!',
-          'contents': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolorum incidunt cum facilis veniam deleniti ullam alias excepturi pariatur quod sunt quasi delectus, maiores rerum provident vero expedita et earum adipisci! Qui enim laboriosam sequi quibusdam? Nam sint commodi tenetur veritatis tempora eaque nulla voluptatibus.',
+          'contents': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio sit nobis aut facilis praesentium, nihil, commodi dignissimos illum vitae. Quasi vel neque aspernatur ex placeat vero! Voluptates, temporibus ratione tempore atque et consequatur distinctio quisquam, dolore expedita assumenda non, facere magnam debitis quae! Veniam amet consequatur ab ipsum beatae dolor.',
           'type': 'RELEASED',
           'photoUrl': 'http://lorempixel.com/287/300',
           'created': 1494329859282
         },
         {
-          'id': '31f5cad8-ffa4-4789-8216-37c0ab7f03a0',
-          'title': 'title_3',
+          'id': '567fc68a-0c6a-4dc6-948b-cbc002738839',
+          'title': 'W niedzielę schronisko zamknięte',
+          'contents': 'Lorem ipsum ptates, temporibus ratione tempore atque et consequatur distinctio quisquam, dolore expedita assumenda non, facere magnam debitis quae! Veniam amet consequatur ab ipsum beatae dolor.',
+          'type': 'RELEASED',
+          'photoUrl': 'http://lorempixel.com/200/200',
+          'created': 1494329863111
+        },
+        {
+          'id': 'd8728661-d429-4e96-893a-75c0f89af76e',
+          'title': 'Kto tu rządzi?',
           'contents': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio sit nobis aut facilis praesentium, nihil, commodi dignissimos illum vitae. Quasi vel neque aspernatur ex placeat vero! Voluptates, temporibus ratione tempore atque et consequatur distinctio quisquam, dolore expedita assumenda non, facere magnam debitis quae! Veniam amet consequatur ab ipsum beatae dolor.',
           'type': 'RELEASED',
           'photoUrl': 'http://lorempixel.com/200/200',
           'created': 1494329863111
         }],
-//        news: [],
+        // news: [],
+        errors: [],
         loading: false
       }
     },
     created () {
-//      this.fetchData()
+      // this.fetchData()
     },
     computed: {
       convertTimeStamp () {
@@ -78,7 +81,6 @@
           })
       },
       shortenContent (item) {
-        console.log(item)
         const MAX_WORDS = 30
         const words = item.contents.split(' ')
         const wordCount = words.length
@@ -101,46 +103,39 @@
   @import "../assets/styles/variables.sass"
 
   .news-item
-    border-bottom: 1px solid $gray
-    @media (min-width: 768px)
-      margin-bottom: 10px
-
-  .news-date
-    margin-top: 0
-    text-align: left
-    margin: 25px 0 10px 0
-    @media (min-width: 768px)
-      text-align: right
+    padding: 2rem 0
+    border-bottom: 2px solid $gray
+    @media (min-width: 1024px)
+      position: relative
+      border: 2px solid $gray
+      padding: 2rem
+      margin-bottom: 1rem
 
   .news-title
-    text-align: center
     font-size: 2rem
-    margin-bottom: 16px
     text-decoration: underline
-
-  .news-title a
-    color: black
-
-  .content-wrapper
-    padding-right: 40px
+    a
+      color: black
+    @media (min-width: 1024px)
+      display: inline
 
   .news-image
+    max-width: 100%
     width: 345px
     height: 278px
-    margin-bottom: 10px
-    max-width: 100%
-    @media (min-width: 768px)
-      margin-bottom: 0
+    @media (min-width: 1024px)
+      float: left
+      margin: -2rem 2rem -2rem -2rem
 
-  .wrapper
-    margin-bottom: 20px
+  .news-date
+    text-align: left
+    @media (min-width: 1024px)
+      float: right
+
+  .news-button
+    border-color: $white
+    text-transform: none
     text-align: center
-    @media (min-width: 768px)
-      text-align: right
-      position: absolute
-      bottom: 0
-      right: 40px
-
-  a
-    color: #fff
+    @media (min-width: 1024px)
+      float: right
 </style>
