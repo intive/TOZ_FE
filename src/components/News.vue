6893723bb8d47b73921154180fec227bf7032801
Tomasz Wołoszyk <Tomek1592@gmail.com>
2017-05-31 22:46:37 +0200

    redesign/news

--- src/components/News.vue
+++ src/components/News.vue
@@ -5,26 +5,40 @@
       <div class="errors" v-if="errors.length">
         <h2 v-for="error of errors">{{ error.message }}</h2>
       </div>
-        <h5 class="news-header">Aktualności:</h5>
-        <hr>
-      <div v-for="item of news" :key="item.id" class="news-item clearfix">
-        <div class="news-date">
-          {{ $t('news.added') }} {{ convertTimeStamp }}
+      <h5 class="news-header">{{ $t('news.news') }}</h5>
+      <hr>
+      <div v-for="item of news" :key="item.id" class="row panel-item clearfix" v-show="item.published > Date.now() - 13046400000">
+        <div class="col-3">
+          <img :src="item.imageUrl" alt="" class="panel-img float-left">
         </div>
-        <h2 class="news-title">
-          <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', shortened: false } }">
-            {{ item.title }}
-          </router-link>
-        </h2>
-        <img :src="item.imageUrl" alt="" class="news-image">
-        <p class="news-body"> {{ shortenContent(item) }} </p>
-        <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', shortened: false } }" class="btn news-button">
-          {{ $t('news.readMore') }}
-        </router-link>
+        <div class="col-8 offset-1">
+          <div class="row">
+            <div class="col panel-date">
+              <p>{{ $t('news.added') }} {{ convertTimeStamp(item.published) }}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col panel-title">
+              <u>{{ item.title }}</u>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col panel-content">
+              <p>{{ shortenContent(item.contents) }}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', shortened: false } }" class="btn news-button">
+                {{ $t('news.readMore') }}
+              </router-link>
+            </div>
+          </div>
+        </div>
       </div>
+
     </div>
     <Sidebar></Sidebar>
-
   </div>
 </template>
 
@@ -37,7 +51,7 @@
     name: 'News',
     data () {
       return {
-        news: [],
+        news: {},
         errors: [],
         loading: true
       }
@@ -45,34 +59,52 @@
     created () {
       this.fetchData()
     },
-    computed: {
-      convertTimeStamp () {
-        const date = moment(this.created).locale('pl-PL')
-        return date.format('l')
-      }
-    },
     methods: {
       fetchData () {
         this.$http.get(this.apiUrl + 'news?type=RELEASED&shortened=true')
           .then(response => {
-            this.news = {...response.data}
+            this.news = response.data
             this.loading = false
+            this.news.sort(this.sortNews('published'))
           })
           .catch(error => {
             this.errors.push(error)
             this.loading = false
           })
       },
+      convertTimeStamp (published) {
+        const date = moment(published).locale(this.$t('common.code'))
+        return date.format(this.$t('common.dateFormat'))
+      },
+      sortNews (key, order = 'asc') {
+        return (a, b) => {
+          if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            return 0
+          }
+
+          const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key]
+          const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key]
+
+          let comparison = 0
+          if (varA > varB) {
+            comparison = 1
+          } else if (varA < varB) {
+            comparison = -1
+          }
+
+          return ((order === 'desc') ? (comparison * -1) : comparison)
+        }
+      },
       shortenContent (item) {
         const MAX_WORDS = 30
-        const words = item.contents.split(' ')
+        const words = item.split(' ')
         const wordCount = words.length
 
         if (wordCount > MAX_WORDS) {
           let sliced = words.slice(0, MAX_WORDS)
           sliced.push(' [...]')
           return sliced.join(' ')
-        } else return item.contents
+        } else return item
       }
     },
     components: {
@@ -86,74 +118,42 @@
   @import "../assets/styles/loader.css"
   @import "../assets/styles/variables.sass"
 
-  .wrapper
-    margin-top: 5em
-    height: 100%
-    width: 100%
-  .news-item
-    padding: 1rem
-    border-bottom: 2px solid $gray
-    @media (min-width: 1024px)
-      position: relative
-      border: 2px solid $gray
-      padding: 2rem
-      margin-bottom: 1rem
-  .news-header
+  .panel-content
+    margin: 10px 0 10px -50px
+    text-align: justify
+    p
+      line-height: 1.42
+
+  .panel-date
+    font-size: .5rem
+    margin: 20px 10px 0 0
+    text-align: right
+
+  .panel-img
+    height: 278px
+    margin: 0 10px 0 -15px
+    width: 345px
+
+  .panel-item
+    border: 2px solid $gray
+    margin: 20px 0 20px 0
+
+  .panel-title
+    font-size: 1.2rem
+    font-weight: bold
+    margin: 10px 0 10px -50px
     text-align: left
-    padding: 0
-    margin: 0
-  .news-title
-    font: $font-stack
-    font-size: 2em
-    text-decoration: underline
-    a
-      color: black
-    @media (min-width: 1024px)
-      display: inline
-
-  .news-image
-      max-width: 100%
-      width: 345px
-      height: 278px
-      @media (min-width: 1024px)
-        float: left
-        margin: -2rem 2rem -2rem -2rem
-
-  .news-date
-      text-align: left
-      @media (min-width: 1024px)
-        float: right
 
   .news-button
-      border-color: $white
-      text-transform: none
-      text-align: center
-      @media (min-width: 1024px)
-        float: right
+    border-color: $white
+    margin: 20px 50px 20px 0
+    text-transform: none
+    text-align: center
+    @media (min-width: 1024px)
+      float: right
 
-  .right-panel
-      height: 100%
-      margin: 5em 5em 0 5.5em
-      padding: 1em 0 0 .8em
-      @media (max-width: 1108px)
-        margin: 0
-      @media (max-width: 768px)
-        display: none
-      @media (min-width: 1200px)
-        margin: 0 auto
-
-      .right-panel-header
-        font-size: 1.8em
-        font-weight: bold
-        line-height: 1.3
-        text-align: left
-
-      .right-panel-text
-        color: #b9b9b9
-        font-size: 1.4em
-        line-height: 1
-        text-align: left
-
-      .last
-        margin-bottom: 50px
+  .wrapper
+    height: 100%
+    margin-top: 5em
+    width: 100%
 </style>
