30b6257361e739179c5d18de317c199f52292c8b
Tomasz Wołoszyk <Tomek1592@gmail.com>
2017-06-01 11:03:42 +0200

    fix/responsiveness of news

--- src/components/News.vue
+++ src/components/News.vue
@@ -7,36 +7,35 @@
       </div>
       <h5 class="news-header">{{ $t('news.news') }}</h5>
       <hr>
-      <div v-for="item of news" :key="item.id" class="row panel-item clearfix" v-show="isNewsExpired(item.published)">
-        <div class="col-3">
+      <div v-for="item of news" :key="item.id" class="row panel-item" v-show="isNewsExpired(item.published)">
+        <div class="col-3 hidden-md-down">
           <img :src="item.imageUrl" alt="" class="panel-img float-left">
         </div>
-        <div class="col-8 offset-1">
+        <div class="col-12 col-sm-9 pt-2 pl-5 pb-2">
           <div class="row">
-            <div class="col panel-date">
-              <p>{{ $t('news.added') }} {{ convertTimeStamp(item.published) }}</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col panel-title">
-              <u>{{ item.title }}</u>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col panel-content">
-              <p>{{ shortenContent(item.contents) }}</p>
+            <div class="col">
+              <p class="float-right">{{ $t('news.added') }} {{ convertTimeStamp(item.published) }}</p>
             </div>
           </div>
           <div class="row">
             <div class="col">
-              <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', shortened: false } }" class="btn news-button">
+              <h4 class="float-left"><u>{{ item.title }}</u></h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <p class="float-left">{{ item.contents }}</p>
+            </div>
+          </div>
+          <div class="row mt-5">
+            <div class="col">
+              <router-link :to="{ name: 'news', params: { id: item.id }, query: { type: 'RELEASED', isShortened: false, isOrdered: true } }" class="btn news-button">
                 {{ $t('news.readMore') }}
               </router-link>
             </div>
           </div>
         </div>
       </div>
-
     </div>
     <Sidebar></Sidebar>
   </div>
@@ -61,11 +60,10 @@
     },
     methods: {
       fetchData () {
-        this.$http.get(this.apiUrl + 'news?type=RELEASED&shortened=true')
+        this.$http.get(this.apiUrl + 'news?type=RELEASED&isShortened=true&isOrdered=true')
           .then(response => {
             this.news = response.data
             this.loading = false
-            this.news.sort(this.sortNews('published'))
           })
           .catch(error => {
             this.errors.push(error)
@@ -82,36 +80,6 @@
         } else {
           return false
         }
-      },
-      sortNews (key, order = 'asc') {
-        return (a, b) => {
-          if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-            return 0
-          }
-
-          const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key]
-          const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key]
-
-          let comparison = 0
-          if (varA > varB) {
-            comparison = 1
-          } else if (varA < varB) {
-            comparison = -1
-          }
-
-          return ((order === 'desc') ? (comparison * -1) : comparison)
-        }
-      },
-      shortenContent (item) {
-        const MAX_WORDS = 30
-        const words = item.split(' ')
-        const wordCount = words.length
-
-        if (wordCount > MAX_WORDS) {
-          let sliced = words.slice(0, MAX_WORDS)
-          sliced.push(' [...]')
-          return sliced.join(' ')
-        } else return item
       }
     },
     components: {
@@ -125,16 +93,11 @@
   @import "../assets/styles/loader.css"
   @import "../assets/styles/variables.sass"
 
-  .panel-content
-    margin: 10px 0 10px -50px
-    text-align: justify
-    p
-      line-height: 1.42
-
-  .panel-date
-    font-size: .5rem
-    margin: 20px 10px 0 0
-    text-align: right
+  .card
+    border: none
+    &:hover
+      background-color: inherit
+      color: inherit
 
   .panel-img
     height: 278px
@@ -145,15 +108,8 @@
     border: 2px solid $gray
     margin: 20px 0 20px 0
 
-  .panel-title
-    font-size: 1.2rem
-    font-weight: bold
-    margin: 10px 0 10px -50px
-    text-align: left
-
   .news-button
     border-color: $white
-    margin: 20px 50px 20px 0
     text-transform: none
     text-align: center
     @media (min-width: 1024px)
