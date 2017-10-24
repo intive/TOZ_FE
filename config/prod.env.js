3b0a2a2e394a0623a4e72951fb133b9e25596d49
Lukasz Roszak <lukasz.roszak@blstream.com>
2017-10-24 12:56:13 +0200

    Update prod.env.js
    Changed production environment configuration

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://dev.patronage2017.intive-projects.com/api/"'
+  API_URL: '"http://104.45.29.37/api/"'
   //API_URL: '"https://intense-badlands-80645.herokuapp.com/"'
 }
